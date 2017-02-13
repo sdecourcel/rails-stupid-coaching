23abf952aae51f95e82dac163f31ae94423b8944
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-13 11:59:29 +0100

    cool

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,21 @@
+class CoachingController < ApplicationController
+  def answer
+    @your_question = params[:query]
+    @coach_answer = coach_answer(@your_question)
+  end
+
+  def ask
+  end
+
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      ''
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/answer' => 'coaching#answer'
+
+  get '/ask' => 'coaching#ask', as: :ask
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
