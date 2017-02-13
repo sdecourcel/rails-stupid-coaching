94cf6d3319e54c7f1b6ecd169f9b83373a75abef
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-13 12:09:12 +0100

    root

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -18,4 +18,17 @@ class CoachingController < ApplicationController
     end
   end
 
+  def coach_answer_enhanced(your_message)
+  answer = coach_answer(your_message)
+  if answer != ""
+    if your_message.upcase == your_message
+      "I can feel your motivation! #{answer}"
+    else
+      answer
+    end
+  else
+    ""
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get '/ask' => 'coaching#ask', as: :ask
 
+  root 'coaching#ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
